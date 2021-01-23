728aab4b417642c223200c43d7e54bcb6dc5db97
Emre KILIC <emre1kilic@outlook.com>
2021-01-24 00:49:53 +0300

    solved reversed

--- SOLs/Program.cs
+++ SOLs/Program.cs
@@ -8,16 +8,8 @@ namespace SOLs
     {
         static void Main(string[] args)
         {
-            var sums = new TwoSums();
-            int[] test1 = new[] { 2, 7, 11, 15 };
-            int target1 = 9;
-            sums.TwoSum(test1, target1);
-            int[] test2 = new[] { 3, 2, 4 };
-            int target2 = 6;
-            sums.TwoSum(test2, target2);
-            int[] test3 = new[] { 3, 3};
-            int target3 = 6;
-            sums.TwoSum(test3, target3);
+            var solution = new Reverse();
+            solution.reverse(1534236469);
         }
     }
 }
--- SOLs/Utils.cs
+++ SOLs/Utils.cs
@@ -5,6 +5,15 @@ namespace SOLs
     public class Utils
     {
         public void PrintHelloWorld() => Console.WriteLine("Hello World");
-        
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
     }
 }
