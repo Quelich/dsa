b53c6d9795bc137709d39b31802a1245b66c9bf8
Quelich <emre1kilic@outlook.com>
2021-06-29 15:17:44 +0300

    added solutions added Quelich.Algorithmus in order to query explicit algorithms for Leetcode challenges

---
+++ SOLs/MaximumSubarray.cs
@@ -0,0 +1,32 @@
+using System;
+namespace SOLs
+{
+    public class MaximumSubarray
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = {-2,1,-3,4,-1,2,1,-5,4 };
+            Console.WriteLine(MaxSubArray(input));
+        }
+        public static int MaxSubArray(int[] nums)
+        {
+            int max_so_far = int.MinValue,
+                max_ending_here = 0;
+            
+            foreach (var variable in nums)
+            {
+                max_ending_here = max_ending_here + variable;
+			
+                if (max_so_far < max_ending_here)
+                    max_so_far = max_ending_here;
+			
+                if (max_ending_here < 0)
+                    max_ending_here = 0;
+            }
+		
+            return max_so_far;
+        }
+
+       
+    }
+}
---
+++ SOLs/Quelich.Algorithmus/euclid_algorithm.cs
@@ -0,0 +1,7 @@
+namespace SOLs.Quelich.Algorithmus
+{
+    public class euclid_algorithm
+    {
+        
+    }
+}
